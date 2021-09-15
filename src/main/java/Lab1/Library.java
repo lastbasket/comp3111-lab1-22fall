07a04fe0dd3ec7c565b770f10d0b17d432c28be3
lastbasket <c1135949168@gmail.com>
2021-09-16 00:16:34 +0800

    modify Library.java

---
+++ src/main/java/Lab1/Library.java
@@ -0,0 +1,12 @@
+package Lab1;
+
+public class Library {
+    public static void main(String arg[]){
+        System.out.println("Hello, World!");
+    }
+    public boolean someLibraryMethod(){
+        return true;
+    }
+}
+
+
---
+++ src/test/java/Lab1/LibraryTest.java
@@ -0,0 +1,12 @@
+package Lab1;
+
+import junit.framework.TestCase;
+
+public class LibraryTest extends TestCase {
+
+    public void testMain() {
+    }
+
+    public void testSomeLibraryMethod() {
+    }
+}
